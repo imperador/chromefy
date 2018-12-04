4cb58b3bd3ae6d55201a536f95ca9e497f8e3772
Taron Ramsay <5731289+zZSETHzz@users.noreply.github.com>
2018-12-04 16:16:15 +0000

    Added files from Chromefy Black Beta v1.1.1

---
+++ fix_grub.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#Script to correct grub problem
+#If installing to a system partition, must be ran from ChromiumOS Live USB (otherwise, any Linux distro is fine)
+#If installing to a system partition, You must install ChromiumOS on your Hard Drive before running this script
+
+#Parameters:
+#1 - Disc where ChromeOs is installed
+
+function fix_grub {
+    sync
+    umount /home/chronos/EFI 2>/dev/null
+}
+
+# Get the partition of the EFI
+if [ ! -d /home/chronos/EFI ]; then mkdir /home/chronos/EFI; fi
+if [ -z "$1" ]; then echo "Missing Disk"; exit 1; fi
+EFIPART=`sfdisk -lq "$1" 2>/dev/null | grep "^""$1""[^:]" | awk '{print $1}' | grep [^0-9]12$`
+
+# Try to mount the EFI partition
+mount "$EFIPART" /home/chronos/EFI -o loop,rw  2>/dev/null
+if [ ! $? -eq 0 ]; then echo "Disk $1 does not have a EFI partition (corrupted?)"; fix_grub; fi
+
+# Gets the UUID at the grub file
+OLD_UUID=`cat /home/chronos/EFI/efi/boot/grub.cfg | grep -m 1 "PARTUUID=" | awk '{print $15}' | cut -d'=' -f3`
+
+# Changes the grub configuration to point to the right partition
+PARTUUID=`sfdisk --part-uuid "$1" 3`
+sed -i "s/$OLD_UUID/$PARTUUID/" /home/chronos/EFI/efi/boot/grub.cfg
+
+echo "Partition $OLD_UUID changed to $PARTUUID"
+echo "You can reboot your PC!"
+
+fix_grub
+
